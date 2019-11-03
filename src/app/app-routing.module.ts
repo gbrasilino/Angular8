94f97b74132d8c31e3d2dd9124e79db1beccb05e
Gustavo <gustavo.brasilino@gmail.com>
2019-11-03 18:42:18 -0300

    Filmes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListaFilmeComponent} from './filme/lista-filme/lista-filme.component';
+import {HomeComponent} from './home/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listar-filmes', component: ListaFilmeComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
